068d9069e4d5060ec9780db8d78f4e31b8409f14
Roopal <roopalsaxena12@gmail.com>
2020-10-26 17:47:41 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/misapplication/LoginActivity.java

--- app/src/main/java/com/example/misapplication/OtpverficationActivity.java
+++ app/src/main/java/com/example/misapplication/OtpverficationActivity.java
@@ -1,28 +1,31 @@
 package com.example.misapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class OtpverficationActivity extends AppCompatActivity {
-    EditText editTextOtp;
-    Button verfiybtn;
+
+    Button buttonverify;
+    EditText otpverify;
+    public final int REQUEST_CODE_SMS_PERMISSIONS = 1001;
+    String URLline;
+    SharedPreferences sharedPrFRegis;
+    String Rempcode,Rname,Rstatus,Rotp,Rmobile;
+    public static String OTpInpput=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(OtpverficationActivity.this, new String[]{android.Manifest.permission.RECEIVE_SMS}, REQUEST_CODE_SMS_PERMISSIONS);
         setContentView(R.layout.activity_otpverfication);
-        editTextOtp=findViewById(R.id.enterotpcode);
-        verfiybtn=findViewById(R.id.btn_get_data);
-        verfiybtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              Intent intent=new Intent(OtpverficationActivity.this,ChangePassActivity.class);
-              startActivity(intent);
-            }
-        });
+
+        otpverify=findViewById(R.id.enterotpcode);
+        buttonverify=findViewById(R.id.btn_get_data);
     }
 }
